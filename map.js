63d6d11b9228730c9eb7d5e5ffc844613d0cbf47
ysfkcr <yusufkecer0007@gmail.com>
2023-01-31 12:48:24 +0300

    add map

---
+++ map.js
@@ -0,0 +1,7 @@
+let map = new Map();
+map.set(07, "Antalya");
+map.set(06, "Ankara");
+map.set(34, "İstanbul");
+map.set([1, 2, 3, 4], "Kıbrıs");
+console.log(map.get(07));
+console.log(map.get([1, 2, 3, 4]));//reference 
